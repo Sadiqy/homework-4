44f388fa0c0e7cf15bc2c17101ff3bb0fea0c717
Git Sadiq <abdulaisadiq@live.com>
2020-02-01 15:25:05 -0800

    Started over and modified JS code, added new styling more better understanding

--- script.js
+++ script.js
@@ -1,121 +1,151 @@
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
 
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
+let stopWatch = 60;
 
-const progress =  document.getElementById("progress");
-const scoreContainer =  document.getElementById("scoreContainer");
 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+        stopWatch += 10;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+//
+let timeEl = document.querySelector(".time");
+
+let timerInterval = setInterval(function() {
+    stopWatch--;
+    timeEl.textContent = stopWatch + " seconds left.";
+    if(stopWatch === 0) {
+        clearInterval(timerInterval);
+        showScores();
+        addPersonToList();
+      }
+}, 1000);
+
+
+function setTime() {
+    timerInterval;
+
+}
+//
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+        addPersonToList();
+        //incorporate the name signing thing into show scores function?
+    }
+    else {
+        // show question
+        let element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        let choices = quiz.getQuestionIndex().choices;
+        for(let i = 0; i < choices.length; i++) {
+            let element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+        setTime();
+    }
+};
+
+function guess(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+
+let scoresheet = document.querySelector("#scoresheet")
+let progress = document.querySelector("#progress")
+
+
+function showScores() {
+    scoresheet.classList.remove("hide");
+    let gameOverHTML = "<h1>Score</h1>";
+    gameOverHTML += "<h2 id='score'> Your score: " + quiz.score + " correct out of 5!</h2>";
+    let element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+    keyEventsEl.classList.remove(".hide");
+    progress.classList.add(".hide");
+    //Add score addition form)
+};
+
+
+function addPersonToList(event) {
+  event.preventDefault();
+  let name = nameEl.value;
+  let li = document.createElement("li");
+  li.id = people.length;
+  li.innerHTML = name + " got " + quiz.score + " correct out of 5.";
+  people.push({ name: name });
+  peopleListEl.append(li);
+}
+
+let addBtn = document.querySelector('#add-btn');
+let nameEl = document.querySelector("#name");
+let peopleListEl = document.querySelector("#people-list");
+let modalEl = document.querySelector("#modal-container");
+let modalNameEl = document.querySelector("#modal-name");
+let descriptionEl = document.querySelector("#description");
+let closeEl = document.querySelector(".close");
+
+
+addBtn.addEventListener("click", addPersonToList);
+
+
+let people = [{ name:"" }];
+
+
+
+// create questions here
 let questions = [
-    {
-        question : "How many castles are on the Wall?",
-
-        choiceA : "Seven",
-        
-        choiceB : "Eleven",
-        
-        choiceC : "Nineteen",
-
-        correct: "C"
-
-    },
-    {
-        question : "How many times did Tyrion stand trial?",
-
-        choiceA : "Only once",
-        
-        choiceB : "Three times",
-        
-        choiceC : "Twice",
-
-        correct: "B"
-
-    },
-    {
-        question : "What is the name of Rickon Stark's direwolf?",
-
-        choiceA : "Shaggy Dog",
-        
-        choiceB : "Grey Wind",
-        
-        choiceC : "Snow Wolf",
-
-        correct: "A"
-
-    },
-    {
-        question : "How many castles on the Wall are actually manned?",
-
-        choiceA : "One, Castle Black",
-        
-        choiceB : "Three",
-        
-        choiceC : "Two, Castle Black and East Watch by the sea",
-
-        correct: "B"
-
-    },
+    new Question("How many castles are on the Wall?", ["Seven", "Eleven","Nineteen", "ten"], "Nineteen"),
+    new Question("How many times did Tyrion stand trial?", ["only once", "Three times", "Twice", "Never"], "Three times"),
+    new Question("What is the name of Rickon Stark's direwolf?", ["Shaggy Dog", "Grey Wind","Snow Wolf", "Wild Ice"], "Shaggy Dog"),
+    new Question("How many castles on the Wall are actually manned?", ["One, Castle Black", "Three", "Two, Castle Black and East Watch by the sea", "Five"], "Three"),
+    new Question("What is the name of Samwell Tarly's younger brother?", ["Rickon", "Willis", "Dickon", "Beric"], "Dickon")
 ];
 
-let lastQuestionIndex = questions.length - 1;
-let currentQuestionIndex = 0;
-let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
-let TIMER;
-let score = 0;
+// create quiz
+let quiz = new Quiz(questions);
 
-function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    }else{
-        count = 0;
-        // change progress color to red
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-        }else{
-            // end the quiz and show the score
-            clearInterval(TIMER);
-            scoreRender();
-        }
-    }
-
-}
-
-function renderQuestion (){
-
-    let q = question[runningQuestionIndex];
-    question.innerHTML = "<p>" + q.question + "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-}
-
-
-function renderProgress(){
-    for(let qIndex = 0; qIndex <= lastQuestionIndex; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
-    }
-}
-
-// answer is correct
-function answerIsCorrect(){
-    document.getElementById(currentQuestionIndex).style.backgroundColor = "green";
-}
-
-// answer is Wrong
-function answerIsWrong(){
-    document.getElementById(currentQuestionIndex).style.backgroundColor = "red";
-}
-
-//counter render
+// display quiz
+populate();
